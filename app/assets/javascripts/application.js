6e14b5243bf308d4d98aebdc4b369bc2d634431c
Goory al Hamed <gooryalhamed@hotmail.com>
2017-09-20 22:50:23 +0300

    refactor create ajax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-
 $(function () {
-  $('.edit_task input[type=submit]').remove();
-  $('.edit_task input[type=checkbox]').click(function () {
-    $(this).parent('form').submit();
-  });  
+	$('.edit_task input[type=submit]').remove();
+	$('.edit_task input[type=checkbox]').click(function () {
+		$(this).parent('form').submit();
+	});  
 });
