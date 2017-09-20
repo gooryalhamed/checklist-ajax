94b463d0f8caa6215c5f8a91d0a81bf807960faa
Goory al Hamed <gooryalhamed@hotmail.com>
2017-09-20 19:50:41 +0300

    change code syntax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,13 +2,9 @@
 //= require jquery_ujs
 //= require_tree .
 
-jQuery.fn.submitOnCheck = function() {
-	this.find('input[type=submit]').remove();
-	this.find('input[type=checkbox]').click(function() {
-		$(this).parent('form').submit();
-	});
-	return this;
-}
-
-$('.edit_task').submitOnCheck();
-
+$(function () {
+  $('.edit_task input[type=submit]').remove();
+  $('.edit_task input[type=checkbox]').click(function () {
+    $(this).parent('form').submit();
+  });  
+});
